8cfd01e994b1bc7c33163a70beb5d707a04fd14f
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-01-22 20:45:56 +0500

    update js file
    Enjoy

--- Login/script.js
+++ Login/script.js
@@ -1,7 +1,7 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
+const btnPopup = document.querySelector('.butlogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', () => {
@@ -11,31 +11,25 @@ registerLink.addEventListener('click', () => {
 loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
-btnPopup.addEventListener('click', () => {wrapper.classList.add('active-popup');
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
 });
-iconClose.addEventListener('click', () => {  wrapper.classList.remove('active-popup'); });
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
 
 
+const formBoxRegister = document.querySelector('.form-box.Register');
+const formBoxLogin = document.querySelector('.form-box.login');
 
+registerLink.addEventListener('click', () => {
+    formBoxRegister.style.transform = 'translateX(0)';
+    formBoxLogin.style.transform = 'translateX(-400px)';
+});
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const wrapper = document.querySelector('.wrapper');
-//     const loginLink = document.querySelector('.login-link');
-//     const registerLink = document.querySelector('.register-link');
-//     const btnPopup = document.querySelector('.btnLogin-popup');
-//     const iconClose = document.querySelector('.icon-close');
-
-//     registerLink.addEventListener('click', () => {
-//         wrapper.classList.add('active');
-//     });
-
-//     loginLink.addEventListener('click', () => {
-//         wrapper.classList.remove('active');
-//     });
-
-//     btnPopup.addEventListener('click', () => {
-//         wrapper.classList.add('active-popup');
-//     });
-
-//    
-// });
+loginLink.addEventListener('click', () => {
+    formBoxLogin.style.transform = 'translateX(0)';
+    formBoxRegister.style.transform = 'translateX(400px)';
+});
