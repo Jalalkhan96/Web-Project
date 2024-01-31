55cb22b391c0db818834e9a98a67013666b1d721
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-01-31 21:52:33 +0500

    Java Script Here
    Java Script Here

---
+++ Javascript Here/Introduction/script.js
@@ -0,0 +1,14 @@
+let a=4;
+let b=3;
+let name = 'Jalal';
+let Name = 'Jalal';
+let age=19;
+age=20;
+const mjk='jalal khan';
+
+
+console.log('The Sum of a+b is ',a+b);
+console.log( 'Your Name Is', name);
+console.log( 'Your Name Is', Name);
+console.log( 'Your age is ', age);
+console.log( 'Your Full Name', mjk);
