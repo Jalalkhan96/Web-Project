7f99a9a13c0f54a34a7a4369b32e919382e31292
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-01-31 21:52:54 +0500

    Data Type
    Data Type Here

---
+++ Javascript Here/Data Type/script.js
@@ -0,0 +1,16 @@
+let a= 23;
+
+// a=a+1;
+// console.log(a);
+ //a ='23'
+
+// a=a+1;
+// console.log(a);
+//a=true;
+// a=String(a);
+
+a=Boolean(a);
+
+
+console.log('THe value of a', a);
+console.log('The type of a is', typeof(a));
