41ff3a0c573f15a83c11eb6e1b46320eafb14d66
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-02-01 11:37:58 +0500

    Function Here
    Script.js file comment 💝

---
+++ Javascript Here/Function Here/nestedFuction.js
@@ -0,0 +1,14 @@
+function addsquare(a,b) {
+    const sa = square (a);
+    const sb = square (b);
+
+
+    function square(num) {
+        
+
+        return num*num;
+    }
+
+    return sa+sb;
+}
+console.log(addsquare(4,5));
---
+++ Javascript Here/Function Here/script.js
@@ -0,0 +1,38 @@
+function sumOfTwoNumber(a,b) {
+    const sum = a + b;
+    console.log('The Result is :', sum);
+    return sum;
+}
+
+
+const a =12;
+const b =12;
+
+ const result =sumOfTwoNumber(a,b);
+console.log('The result is here :!:',result);
+//-------------->
+
+const c = 3;
+const d = 23;
+
+sumOfTwoNumber(c,d);
+
+//-------------->
+
+const e = -12;
+const f = 9;
+
+sumOfTwoNumber(e,f);
+
+// function square(num) {
+//     return num*num;
+
+// }
+
+
+const square =function (num) {
+    return num*num;
+    
+};
+console.log(square);
+console.log(square(4));
