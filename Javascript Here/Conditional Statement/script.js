24ec54686cc00666f72946090f2629eee99e2c7f
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-02-01 09:29:18 +0500

    Conditional Statement
    Conditional Statement

---
+++ Javascript Here/Conditional Statement/script.js
@@ -0,0 +1,36 @@
+// const age=12;
+
+// if (age>=18) {
+//     console.log('You Can Vote');
+//     if (age>60) {
+//         console.log('You Can Vote again');
+//     }
+// }
+// else{
+//     console.log('You cannot Vote');
+
+//     if (age<9) {
+//         console.log('You Should goto School. Baby!');
+//     }
+// }
+
+// console.log('You Name :');
+// let name = 'Anosh kumar';
+// const marks=56;
+
+// if (marks>90) {
+//     console.log('A+');
+// } else if (marks>80) {
+//     console.log('B+');
+// }else if (marks>60) {
+//     console.log('B');
+// }else{
+//     console.log('Low Marks!',name);
+
+// }
+
+const marks=50;
+
+const result= marks>=40 ? 'Passed' : 'Falied';
+
+console.log('Result is', result);
