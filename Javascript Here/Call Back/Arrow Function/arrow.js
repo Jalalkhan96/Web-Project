54b74f12791b89e8ec7d34ddbb851f86f116913a
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-02-01 14:52:14 +0500

    Arrow Function
    Arrow Function Here

---
+++ Javascript Here/Call Back/Arrow Function/arrow.js
@@ -0,0 +1,12 @@
+const greet = (count) =>{
+for (let i = 0; i < count; i++) console.log('Hello Boyz !!!');
+};
+
+greet(3);
+
+const square = (Num) =>{
+   return Num*Num;
+
+};
+
+console.log(square(3));
