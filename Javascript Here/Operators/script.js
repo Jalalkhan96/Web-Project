6ab662fd3cce1ad09353b038f71183d45b82a174
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-02-01 09:29:53 +0500

    Operators js
    Operators js

---
+++ Javascript Here/Operators/script.js
@@ -0,0 +1,35 @@
+// const firstnumber =3;
+// const secondNumber= 4;
+
+// const result= firstnumber+secondNumber;
+// const aa= firstnumber**secondNumber;
+// const th= firstnumber*secondNumber;
+// const eq= firstnumber==secondNumber;
+
+// console.log(result);
+
+// let a =15;
+// a++;
+// // let c= a-1
+
+
+// console.log('The Result is :', aa);
+// console.log('The Result is :', th);
+// console.log('The Result is :', eq);
+// console.log(c);
+
+
+// const firstNumber =3;
+// const secondnumber= '3';
+
+// const res=firstnumber!=secondnumber;
+
+// console.log('The Result is :', res);
+
+
+const firstnumber = false;
+const secondNumber= false;
+
+const result=firstnumber|| secondNumber;
+
+console.log(result);
