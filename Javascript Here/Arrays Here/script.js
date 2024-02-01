74d5c7a8ffb0346dfccc068aac88ba7e1a5c5b8d
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-02-01 11:10:28 +0500

    Array Here
    Array Here

---
+++ Javascript Here/Arrays Here/script.js
@@ -0,0 +1,58 @@
+
+const nums = [4,1,5,6,8,7,9,4];
+console.log(nums.indexOf('The index is :', 4));
+
+//nums.push('jalal');
+
+// nums.unshift('jalal');
+
+// nums.pop()
+// nums.sort();
+
+const  subArray =nums.slice(2,5);
+
+console.log(subArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const words = ['Apple',
+//     'Banana',
+//     'Cherry',
+//     12,
+//     true,
+//     {
+//         name: 'Jalal'
+//     },
+
+//     function hello() {
+//         console.log('Hello  Boyz!!!');
+//     },
+// ];
+
+
+// console.log('The Length of array is :', words.length);
+
+// const newWords = words;
+
+// newWords[6] = 'kiwi';
+
+// console.log(words);
+// console.log(newWords);
