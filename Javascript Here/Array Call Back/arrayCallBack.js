b8555684ba2a5d102872b0b12c6c42fefe21a098
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-02-01 14:52:59 +0500

    Array Call Back Here
    Array Call Back Here

---
+++ Javascript Here/Array Call Back/arrayCallBack.js
@@ -0,0 +1,17 @@
+const a = [4, 1,,5,5,6,-8,9,4,7];
+
+// const firstNeg = (num) => {
+
+//     return num <0;
+
+// };
+
+// const result= a.findIndex(firstNeg);
+
+// console.log(result);
+
+a.forEach((num,i)=>{
+
+console.log('Array num', num, i);
+
+});
