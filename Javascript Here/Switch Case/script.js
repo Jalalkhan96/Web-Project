095c71cbe213604c9168391318b54803aa077af3
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-02-01 09:31:05 +0500

    Switch JS
    Switch JS

---
+++ Javascript Here/Switch Case/script.js
@@ -0,0 +1,21 @@
+const grade = 'D';
+
+switch (grade) {
+    case 'A':
+        console.log('A -> very Good');
+        break;
+
+    case 'B':
+        console.log('B -> Good,Keep learning');
+        break;
+
+    case 'C':
+        console.log('C -> Need Improvement');
+        break;
+
+    case 'D':
+        console.log('D -> Hard Word Needed');
+        break;
+    default:
+        console.log('Not Matched');
+}
