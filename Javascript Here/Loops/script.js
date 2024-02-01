7bbdc6856783ca122c45432d36f6c49276b56040
Jalalkhan96 <135963102+Jalalkhan96@users.noreply.github.com>
2024-02-01 09:49:52 +0500

    Loops Hee
    Loops Here Boy

---
+++ Javascript Here/Loops/script.js
@@ -0,0 +1,31 @@
+// for (let index = 1; index < 5; index++) {
+//     console.log('Jalal',index);    
+// }
+
+//while Loop here Boy!!!
+// let step =0;
+
+// while (step<5) {
+//     console.log("Step is",step);
+//     step++;
+// }
+
+// let step = 6;
+// do { 
+//     console.log("step",step);
+//     step += 1;
+// } while (step < 5);
+
+
+//Break here Boy!!!
+let step =0;
+
+while (step<5) {
+    step++;
+    if (step==2) {
+        continue;
+    }
+
+    console.log("Step is",step);
+   
+}
